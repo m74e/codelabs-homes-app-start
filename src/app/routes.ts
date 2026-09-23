1ab996837c5decfa71fec7635a6dc9cce5985c2e
Muhammed Al-lami <105250911+m74e@users.noreply.github.com>
2026-09-23 16:05:32 +0300

    Add route configuration for home component in router setup

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,6 +1,10 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 
-const routeConfig:Routes=[];
+const routeConfig:Routes=[{
+    path:'home',
+    component:HomeComponent,
+    title:'Home Page'
+}];
 
 export default routeConfig;
