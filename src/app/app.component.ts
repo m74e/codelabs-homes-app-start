c5916ce1bff83fe83f42fa8da2db2ee76d0f2115
Muhammed Al-lami <105250911+m74e@users.noreply.github.com>
2026-09-23 16:01:12 +0300

    Refactor app component to use router outlet and update route configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { HomeComponent } from "./home/home.component";
+import { RouterModule } from "@angular/router";
 @Component({
   standalone: true,
   selector: "app-root",
@@ -8,14 +9,14 @@ import { HomeComponent } from "./home/home.component";
       <img class="brand-logo" src="/assets/logo.svg" alt="brand-logo" />
     </header>
     <section class="content">
-      <app-home></app-home>
-      </section>
+<router-outlet></router-outlet>
+  </section>
       <section class="results">   
       <!-- <app-housing-location></app-housing-location> -->
     </section>
   </main>`,
   styleUrls: ["./app.component.css"],
-  imports: [HomeComponent],
+  imports: [HomeComponen, RouterModule],
 })
 export class AppComponent {
   title = "homes";
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -2,3 +2,5 @@ import {Routes} from '@angular/router';
 import {HomeComponent} from './home/home.component';
 
 const routeConfig:Routes=[];
+
+export default routeConfig;
